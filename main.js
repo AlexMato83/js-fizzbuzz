a81323ff65f1ec1b69a587f7ae7d8ff242fa116e
AlexMato <matontialessandro0@gmail.com>
2020-03-26 14:41:08 +0100

    terminato

--- main.js
+++ main.js
@@ -6,10 +6,26 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi” FizzBuzz”.*/
 
 
 //stampare tutti i numeri da 1 a 100
-for (var tutti = 1; tutti <= 100; tutti++){
-console.log(tutti);
-}
+// for (var tutti = 1; tutti <= 100; tutti++){
+// console.log(tutti);
+// }
 //sostituire "Fizz" ai multipli di 3
+
+for (var i = 1; i <= 100;i++){
+var prova = i;
+ if (i % 3 == 0 && i % 5 == 0) {
+   prova = "FizzBuzz";
+ } else if (i % 5 == 0){
+   prova = "Buzz";
+ } else if (i % 3 == 0){
+   prova = "Fizz";
+ }
+console.log(prova);
+var totale = document.getElementById("asd").innerHTML;
+document.getElementById("asd").innerHTML = totale + " " + prova;
+}
+
+
 
 
 //sostituire "Buzz" ai multipli di 5
